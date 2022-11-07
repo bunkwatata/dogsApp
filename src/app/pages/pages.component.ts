23b8cacbd13b2094e063c1b94f8f661575b868ed
Krzysztof Kolasinski <kkolasinski92@gmail.com>
2022-11-07 02:04:42 +0100

    Added Auth and NoAuth Guards

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStorageService } from '@core/services/auth-storage.service';
 import { environment } from 'environments/environment';
 
 @Component({
@@ -8,5 +10,15 @@ import { environment } from 'environments/environment';
 export class PagesComponent {
   get appName(): string {
     return environment.appName;
+  }
+
+  constructor(
+    private readonly authStorageService: AuthStorageService,
+    private readonly router: Router
+  ) {}
+
+  onLogout(): void {
+    this.authStorageService.removeSessionData();
+    this.router.navigate(['auth']);
   }
 }
